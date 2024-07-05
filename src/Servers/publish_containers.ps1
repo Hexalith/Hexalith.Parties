6de6cb226fdccc1b619cf245273c04dc5ebb9483
Jérôme Piquot <jpiquot@fiveforty.fr>
2024-07-05 13:59:10 +0200

    fix build container

--- src/Servers/publish_containers.ps1
+++ src/Servers/publish_containers.ps1
@@ -2,7 +2,7 @@ az login --tenant christofle.com
 az acr login -n christofle
 $version=(minver -t v -v e -d preview)
 cd ./Hexalith.Server.Parties
-dotnet restore --no-cache
+dotnet restore --no-cache --force --disable-build-servers --force-evaluate  
 dotnet build --no-restore --disable-build-servers --no-incremental -c Release 
 dotnet publish --no-build /t:PublishContainer -c Release -p ContainerRegistry="christofle.azurecr.io" -p ContainerImageTag=$version
 pause
